a86ef53d1847475ae25cc2b696376e4e787b6df6
Derrick Chea <derrick.c.chea@gmail.com>
2018-06-09 21:09:08 -0400

    Add files via upload
    Complete interactive website using JavaScript, J Query, HTML, and Css

--- Script.js
+++ Script.js
@@ -1,4 +1,10 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width"
+
 particles('particles-js'),
 
+var x = document.getElementById('IMG');
+x.src = "banner3.jpg";
+document.body.mac(x);
 
 console.log;
